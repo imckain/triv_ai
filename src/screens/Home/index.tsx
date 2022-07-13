878f5a68341bbdeb0c11fa52d05e405cd068c4fd
imckain <ianmmckain@gmail.com>
2022-07-12 22:44:26 -0400

    WIP homepage functionality

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,32 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState } from 'react';
-import { Platform, PlatformAndroidStatic, PlatformIOSStatic, PlatformMacOSStatic, PlatformWebStatic, PlatformWindowsOSStatic, Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { TextInput, Platform, PlatformAndroidStatic, PlatformIOSStatic, PlatformMacOSStatic, PlatformWebStatic, PlatformWindowsOSStatic, Text, View, Image, StyleProp, ViewStyle, FlatList, SafeAreaView } from 'react-native';
 import { Container } from '../../components';
 import { Header } from '../../components/Header';
 import { Details } from '../Details';
 import { styles } from './styles';
+import * as Result from '../../hooks/temp.json';
+import { useEffect } from 'react';
+
+// type Answer = {
+//   id: string,
+//   input: string,
+// };
 
 export const Home: React.FC = () => {
   const [modalState, setModalState] = useState(false);
+  const [result, setResult] = useState<typeof Result.result>();
+  const [userInput, setUserInput] = useState('');
+  const [answer, setAnswer] = useState([
+    {
+      id: '1',
+      userInput: 'TEST',
+    },
+  ]);
+
+  useEffect(() => {
+    setResult(Result.result);
+  }, [result]);
 
   const showModal = (state: boolean) => {
     return state === true
@@ -27,13 +46,71 @@ export const Home: React.FC = () => {
       : <></>;
   };
 
+  const checkWebStyles = (mobileStyle: StyleProp<ViewStyle>, webStyle: StyleProp<ViewStyle>, platform: PlatformIOSStatic | PlatformAndroidStatic | PlatformWindowsOSStatic | PlatformMacOSStatic | PlatformWebStatic) => {
+    return platform.OS === 'web'
+      ? webStyle
+      : mobileStyle;
+  };
+
+  const renderAnswer = (el: string, state: { userInput: string; }[]) => {
+    if (state[0].userInput.includes('TEST')) {
+      return <></>;
+    }
+    return (
+      <View style={styles.answerView}>
+        <Text style={styles.answerText} >{el != null ? el : 'NULL'}</Text>
+      </View>
+    );
+  };
+
+  const hydrateAnswers = useCallback((param: string, state: { id: string; userInput: string; }[]) => {
+    if (state[0].userInput.includes('TEST')) {
+      setAnswer([{ id: (state.length + 1).toString(), userInput: param }]);
+    }
+    setAnswer([...state, { id: (state.length + 1).toString(), userInput: param }]);
+  }, []);
+
+  const tempShow = (el: { id: string; userInput: string; }[]) => {
+    el.map((a, i) => {
+      console.log(a.userInput);
+      return (
+        <Text key={i + 1} style={{ color: '#fff' }}>{a.userInput}</Text>
+      );
+    });
+  };
+
+  // useEffect(() => {
+  //   hydrateAnswers(userInput, answer);
+  // }, []);
+
   return (
     <Container>
-      <View style={styles.container}>
+      <View style={checkWebStyles(styles.containerMobile, styles.containerWeb, Platform)}>
         {showHeader(Platform)}
-        <View style={styles.homePage}>
+        <View style={checkWebStyles(styles.homePageMobile, styles.homePageWeb, Platform)}>
+          <Text style={{ color: '#fff', fontSize: 42, letterSpacing: 16, fontWeight: '700' }}>{result != null ? result.type.toUpperCase() : null}</Text>
           {showModal(modalState)}
-          <Text>Home Screen</Text>
+          <View style={styles.triviaContent}>
+            <View style={styles.imageContainer}>
+              <Image style={{ borderRadius: 10, paddingVertical: 20 }} source={{ width: 300, height: 300, uri: result?.urls[0] }} />
+            </View>
+            <SafeAreaView style={styles.answerContainer}>
+              <FlatList
+                ListFooterComponent={
+                  <View style={styles.inputView}>
+                    <TextInput returnKeyType="done" keyboardAppearance="dark" clearButtonMode="always" style={styles.input} value={userInput} onChangeText={input => setUserInput(input)} onEndEditing={() => { hydrateAnswers(userInput, answer); }} textAlign={'center'} placeholder="Answer" placeholderTextColor={'#909090'} />
+                  </View>
+                }
+                contentContainerStyle={styles.gameplayContainer}
+                data={answer}
+                extraData={answer}
+                renderItem={({ item }) => renderAnswer(item.userInput, answer)}
+                keyExtractor={(item, _index) => item.id}
+              />
+              <Text style={{ color: '#fff' }}>{userInput}</Text>
+              {tempShow(answer)}
+            </SafeAreaView>
+          </View>
         </View>
       </View>
     </Container>
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
+  containerMobile: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -10,15 +10,75 @@ export const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: '#1f1f1f',
   },
-  homePage: {
+  containerWeb: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+    width: '100%',
+    backgroundColor: '#1f1f1f',
+  },
+  homePageMobile: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
     width: '100%',
     maxWidth: 800,
     borderColor: 'black',
     borderWidth: 2,
     backgroundColor: '#000000',
+    paddingTop: 16,
+  },
+  homePageWeb: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: 800,
+    borderColor: 'black',
+    borderWidth: 2,
+    backgroundColor: '#000000',
+    paddingTop: 88,
+  },
+  triviaContent: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: '100%',
+  },
+  imageContainer: {
+    paddingVertical: 18,
+  },
+  gameplayContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  answerContainer: {
+    justifyContent: 'space-evenly',
+  },
+  answerView: {
+    paddingVertical: 12,
+    borderWidth: 1,
+    borderBottomColor: '#fff',
+    width: '100%',
+  },
+  answerText: {
+    color: '#fff',
+  },
+  inputView: {
+    paddingVertical: 22,
+  },
+  input: {
+    backgroundColor: '#000000',
+    width: 300,
+    height: 60,
+    borderRadius: 5,
+    color: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#303030',
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
